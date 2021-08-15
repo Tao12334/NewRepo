5e83efae204cd9d88e4e18bbca4fe7ad798bfa30
Tao12334 <86705380+Tao12334@users.noreply.github.com>
2021-08-15 12:50:17 +0700

    Add files via upload

---
+++ Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	//การหมุนกล้อง
+	void Update () 
+	{
+		this.transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}	
